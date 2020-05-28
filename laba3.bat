934bd3c9997aef3a94ad33f54f59a88583fd57dc
Mamontova <amamontova97@gmail.com>
2020-05-28 10:07:43 +0300

    just work

---
+++ laba3.bat
@@ -0,0 +1,26 @@
+echo off
+md mamontova 
+cd mamontova
+md anastasia
+cd anastasia
+md andreevna
+cd ..
+pause
+echo> 12111997.txt
+cd anastasia
+cd andreevna
+echo> asus.txt
+cd ..
+cd ..
+cd ..
+pause
+del mamontova  /S/Q/F
+pause
+cd mamontova 
+cd anastasia
+rd andreevna
+cd ..
+rd anastasia
+cd ..
+rd mamontova 
+pause
